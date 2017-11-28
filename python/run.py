f733bcf34e5b5652019fa2fbf53f5e91f99649d0
80sCruiser <js83@rice.edu>
2017-11-28 01:15:56 -0600

    fixed append error

--- python/run.py
+++ python/run.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scoring import basicFight, fromFilePopScoring
 from geneticAlgorithm import basicPopCrossOver, totalPopCrossOver
+from hillClimb import miteSelfByGeneClimb
 
 np.set_printoptions(threshold=np.nan)
 
@@ -10,17 +11,21 @@ threes = np.ones((1,50)) * 3
 initPop = np.load('gen0.npy')
 gen = 1
 while True:
+	print "gen " + str(gen - 1)
+	print initPop
+	
 	newPop = totalPopCrossOver(initPop)
 	newPopSize = newPop.shape[0]
 	filteredPop = np.zeros((0, 50))
 	fileScores = fromFilePopScoring(newPop)
 	scores = []
 	for i in range(len(fileScores)):
-		scores.append = (newPop[i, :], fileScores[i, 0])
+		scores.append((newPop[i, :], fileScores[i, 0]))
 	scores.sort(key=lambda tup: tup[1], reverse=True)
 	for mite, score in scores[:min(len(scores), 30)]:
 		filteredPop = np.vstack((filteredPop, mite))
-	np.save("gen" + str(gen) + ".npy")
-	np.save("savedPop.npy")
+	np.save("gen" + str(gen) + ".npy", filteredPop)
+	np.save("savedPop.npy", filteredPop)
+	initPop = filteredPop
 	gen += 1
 
